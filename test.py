aba8e5251ad528a73714d5dfa6a8d6cb27d758cf
Anthony Krichevskiy <akrichevskiy1@babson.edu>
2019-04-19 01:15:43 -0400

    Working on song inputs from a user producing different generative art

--- test.py
+++ test.py
@@ -11,14 +11,29 @@ import sys
 client_credentials_manager = SpotifyClientCredentials('2c49178b39bc423dbb44f1ce220c12bb', 'ecb09ffa5ffe46d2844e1bd07c75659e')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-if len(sys.argv) > 1:
-    tid = sys.argv[1]
-else:
-    tid = 'spotify:track:4TTV7EcfroSLWzXRY6gLv6'
+def test_confirmation(confirmation):
+    if confirmation == 'Y':
+        uri = results['tracks']['items'][1]['uri']
+        return
+    elif confirmation == 'N':
+        print('Noooo')
+        return
+    else:
+        print('Please answer the question again. Type Y or N and then hit enter.')
+        confirmation = input('Is the song ' + results['tracks']['items'][1]['name'] + ' by ' + results['tracks']['items'][1]['artists'][0]['name'] + '? Y/N \n')
+        test_confirmation(confirmation)
 
-start = time.time()
-analysis = sp.audio_analysis(tid)
-delta = time.time() - start
-print(sp.audio_features(tid))
+search_input = '"' + input('What song do you want to dance to? Please enter the track title. \n') + '"'
+results = sp.search(q=search_input, limit=3, type='track')
+results['tracks']['items'][1]['artists'][0]['name']
+confirmation = input('Is the song ' + results['tracks']['items'][1]['name'] + ' by ' + results['tracks']['items'][1]['artists'][0]['name'] + '? Y/N \n')
+test_confirmation(confirmation)
+print(uri)
+#tid = 'spotify:track:4bHsxqR3GMrXTxEPLuK5ue'
+#print(sp.track(tid))
+#start = time.time()
+#analysis = sp.audio_analysis(tid)
+#delta = time.time() - start
+#print(sp.audio_features(tid))
 #print(json.dumps(analysis, indent=4))
 #print ("analysis retrieved in %.2f seconds" % (delta,))
